09c5183351b2be5a7c8b8d3c2c2beacda4829efc
Kyle McGourty <kyle.mcgourty@gmail.com>
2018-10-01 11:52:02 -0700

    begin 3.3

---
+++ Chapter 3.3.py
@@ -0,0 +1,37 @@
+
+
+#Chapther 3.3
+
+
+#3.3.1
+
+answer = "1st          E" \
+\
+         "2nd       A  E"\
+\
+        "3rd           E" \
+    "                A   S"\
+\
+        "4th           E"\
+                   " A   S Y"\
+\    
+        "5th           E S" \
+         "           A  Q  Y"\
+\
+        "6th             E S "\
+        "              A  Q  UY" \
+ \              
+        "7th                S" \
+         "                E  U" \
+         "              A  Q T  Y" \
+ \
+         "8th                S" \
+         "                E  U" \
+         "              A  IQ T  Y" \
+\
+         "9th                S" \
+         "                EO    U" \
+         "              A I Q  T  Y" \
+\
+
+
