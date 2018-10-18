e16409f5f47edca56d3bca3652d4567e4bdfc9bc
Kyle McGourty <kyle.mcgourty@gmail.com>
2018-10-18 10:26:37 -0700

    finished 4.1

--- Chapter 3.3.py
+++ Chapter 3.3.py
@@ -237,3 +237,5 @@ answer = "Let Cn be the internal path length of a Red Black BST. The average len
 answer = "Red black BST are nearly always perfectly balanced. Perfectly balanced trees will always be less than or equal to" \
          "the height of binary search trees. For large n, the average height of a BST is near 2.99 lg n. For Red Black BST's," \
          "their height is not larger than 2 lg n."
+
+
--- Chapter 4.1.py
+++ Chapter 4.1.py
@@ -96,3 +96,133 @@ class DFS_Connected_Vertex_Finder:
             """a vertext where all adjacent vertices are marked"""
             return v
 
+#4.1.12
+
+answer = "One cannot calculate the distance between two nodes at least one of them is not the root."
+
+#4.1.13
+
+
+class BFS_DistanceTo:
+    def __init__(self, G, source=0):
+        self.marked = [None]*G.V()
+        self.edgeTo = [None]*G.V()
+        self.distTo = [None] * G.V()
+        self.source = source
+
+    def bfs(self, G, s):
+        queue = list()
+        self.marked[s] = True
+        self.edgeTo[s] = s
+        queue.append(s)
+
+        while len(queue)>0:
+            vertex = queue.pop(0)
+            count = 0
+            while True:
+                previous = self.edgeTo[vertex]
+                count += 1
+                if previous == s:
+                    break;
+            self.distTo[vertex] = count
+            for w in G.adj(vertex):
+                if self.marked[w] == None:
+                    self.edgeTo[w] = vertex
+                    self.marked[w] = True
+                    queue.append(w)
+
+    def retrieve_distTo(self, v):
+        return self.distTo[v]
+
+
+
+    def eccentricity_constructor(self,s, G, diameter, radius):
+        max_eccentricities = [None] * G.V()
+
+        self.bfs(G, s)
+
+        self.all_vertices = self.marked[:]
+
+        max_eccentricities[s] = max(self.distTo)
+
+        for vertex in self.all_vertices:
+            self.distTo = [None] * G.V()
+            self.bfs(G, vertex)
+            max_eccentricities[vertex] = max(self.distTo)
+
+        if diameter:
+            return max(max_eccentricities)
+        elif radius:
+            return min(max_eccentricities)
+        else:
+            self.center = max_eccentricities.index(min(max_eccentricities))
+
+    def diameter(self,s, G):
+        return self.eccentricity_constructor(self, s, G, True)
+
+    def radius(self,s, G):
+        return self.eccentricity_constructor(self, s, G, False, True)
+
+    def center(self, s, G):
+        return self.eccentricity_constructor(self, s, G, False, False)
+
+    def wiener_index(self, s, G):
+
+        shortest_paths = list()
+        self.weiner_marked = [None] * G.V()
+        self.all_vertices = self.marked.copy()
+
+        self.bfs(G, s)
+        
+        for vertex in self.all_vertices:
+            self.distTo = [None] * G.V()
+            self.bfs(G, vertex)
+            self.weiner_marked[vertex] = True
+
+            for index, distance in enumerate(self.distTo):
+                if self.weiner_marked[index] == None:
+                    shortest_paths.append(distance)
+
+        return sum(shortest_paths)
+
+
+#4.1.14
+
+answer = "No, the result would be a DFS"
+
+#4.1.16  See 4.1.13
+
+
+#4.1.17 See 4.1.13
+
+#4.1.20 See PDF.
+
+answer = "Runtime is proportional to the length of the cycle"
+
+
+#4.1.26
+
+class SymbolGraph:
+    def __init__(self):
+        self.keys_to_ints = dict()
+        self.ints_to_keys = list()
+
+class DegreesOfSeparation:
+    def __init__(self, G):
+        self.dfs = DepthFirstSearch(G)
+        self.G = G
+
+    def degrees_of_separation(self, sink):
+        vertex = self.G.indexOf(sink)
+        if (self.dfs.hasPathTo(vertex)):
+            for w in self.dfs.pathTo(vertex):
+                print("movie/performer", self.G.nameOf(w))
+
+
+#4.1.27
+
+answer = "Just considering the E edges represented by a bag of linked lists, the memory used is proportional to 32 + 64 E. The array" \
+         "and the linked lists combined use to hold the hold the edges us ~ 8 E * V bytes"
+
+
+#4.1.28 See PDF.
