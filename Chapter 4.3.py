cbff1327858b29ef7c0fb1eb37a2823a61f5fe6d
Kyle McGourty <kyle.mcgourty@gmail.com>
2018-10-29 10:42:25 -0700

    working on 4.3

--- Chapter 4.3.py
+++ Chapter 4.3.py
@@ -34,3 +34,74 @@ answer = "Consider all weights between a cut. Take the maximum valued edged betw
          "cuts of the graph until a maximum spanning tree is formed."
 
 
+#4.3.8
+
+answer = "A cycle is a minimum spanning tree plus one additional edge. Thus the minimum spanning tree of a given cycle of nodes " \
+         "must be formed by removing the edge of maximum weight. Otherwise, it would not be a MST."
+
+#4.3.9
+
+
+class Edge:
+    def __init__(self, v, w, weight):
+        self.v = v
+        self.w = w
+        self.weight = weight
+
+    def edge_weight(self):
+        return self.weight
+
+    def either(self):
+        return self.v
+
+    def other(self, vertex):
+        if vertex == self.v:
+            return self.w
+        if vertex == self.w:
+            return self.v
+
+    def compateTo(self, other_edge):
+        if self.weight < other_edge.weight:
+            return -1
+        elif self.weight > other_edge.weight:
+            return 1
+        else:
+            return 0
+
+
+class EdgeWeightGraph:
+    def __init__(self, V):
+        self.V = V
+        self.E = 0
+        self.adjacency_list = [[] for i in range(self.V)]
+
+    def addEdge(self, edge):
+        v = edge.either
+        w = edge.other(e)
+
+        self.adjacency_list[v].append(edge)
+        self.adjacency_list[w].append(edge)
+
+        self.E +=1
+
+#4.3.11
+
+answer = "Each edge is an object with three integer instance variables. The memory for each edge is 3 * 4 bytes for each integer" \
+         "and then 16 bytes of overhead. Thus the edges consume 28*E bytes. Assuming that the adjacency list is an array of arrays" \
+         "of references to objects. The inner array of objects consumers 24+40*2E bytes. The outer array consumes ~24 + 32V bytes."
+
+#4.3.12
+
+answer = "Prim's algorithm and Proposition J, both demonstrate that a graph's lightest edge will belong to the MST. The heaviest" \
+         "edge in a graph may also belong to a MST: consider a 3 node graph with only two edges. A cycle can be composed of a cut;" \
+         "therefore, it's lightest edge will belong to the MST (by Proposition J)"
+
+#4.3.13
+
+answer = "The problem with this implementation is that it always adds an edge to the most recently added vertex. This compromises" \
+         "the cut principle, because the least weight edge will not be added to the MST"
+
+#4.3.14
+
+answer = "Traverse graph G's edgeTo array until the missing edge is found. Add all weights from the cut to the PQ. Take the minimum" \
+         "weight on the PQ as the new edge on the MST."
