52a7d44e7d21335a2347c98ab9c53f1f8a59f631
Kyle McGourty <kyle.mcgourty@gmail.com>
2018-11-27 12:38:00 -0800

    Finish 5.3

--- Chapter 5.3.py
+++ Chapter 5.3.py
@@ -165,6 +165,17 @@ class RabinKarpSeach:
             h = (self.R * h + ord(text[i])) % self.q
         return h
 
+    def check(self, text, i):
+
+        for char in self.pat:
+            if char != text[i]:
+                return False
+            i +=1
+
+        return True
+
+
+
     def search(self, text):
 
         self.n = len(text)
@@ -178,7 +189,7 @@ class RabinKarpSeach:
             textHash = (textHash * self.R + ord(text[i+self.m])) % self.q
 
             if self.patHash == textHash:
-                return text[i:i+10]
+                return self.check(text, i)
 
         return self.n
 
@@ -187,3 +198,39 @@ class RabinKarpSeach:
 rabinKarp = RabinKarpSeach("reenbeans")
 
 print("result", rabinKarp.search(text))
+
+
+#5.3.11
+
+answer = "A worst case scenario for the Boyer Moore algorithm would be a text of a repeating letter, which is the same " \
+         "letter as the last letter of the pattern. Thus, every letter in the text would be examined."
+
+#5.3.12  See 5.3.10
+
+
+#5.3.13
+
+answer = "When c is the penultimate (second last) occurence and it is also the last character of the sequence, then the right array may" \
+         "be set to the penultimate occurence. This switch is possible because two back-to-back occurences of c exist in the pattern, which" \
+         "voids the necessity of having to move the text twice by just one character."
+
+#5.3.16
+
+answer = """
+A. The brute force pattern processes every sequence of AAAAAA until it reaches the end of the text where it compeletes its search
+by processing AAAAAAAB.
+B. The brute force algorithm processes various lengths of ABABABA until reaching the end of the text without a match.
+"""
+
+#5.3.18
+
+answer = """
+The length of the text that is searched is (n-m+1). The probability of not finding the correct characters in the random string
+over the probability of finding other randoms characters, is the probability the pattern checking in loop will run, which is given
+by (1-R ^-m)/(1-R^-1). Multiplied together, the run time will be less than 2(n-m+1).
+"""
+
+#5.3.19
+
+answer = "When the last character of pattern is the only chaaracter in the text, Boyer-Moore would examine every character of the text."
+
